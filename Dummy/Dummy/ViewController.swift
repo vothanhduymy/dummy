882ce7cd0ea52f02d3485d1e46c2d7be97c51e49
MyVo <my.vo@propzy.com>
2022-06-21 13:54:14 +0700

    init app

---
+++ Dummy/Dummy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dummy
+//
+//  Created by Vo Thanh Duy My on 21/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+}
+
